44e0f32a655b3fb64b37b090f5ede78bfeaa4a32
John Eric Fritz <fritz@orbisoftware.com>
2023-06-02 12:03:59 -0500

    Added a FlatList to display restaurant cards.

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -39,8 +39,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Text variant="label">{name}</Text>
         <Section>
           <Rating>
-            {ratingArray.map((key) => (
-              <SvgXml key={key} xml={star} width={20} height={20} />
+            {ratingArray.map((_data, index) => (
+              <SvgXml key={index} xml={star} width={20} height={20} />
             ))}
           </Rating>
           <SectionEnd>
